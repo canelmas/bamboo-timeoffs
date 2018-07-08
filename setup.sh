f2cd42b389fae0c9e4294aacf070e62c2125c967
Can Elmas <Can.Elmas@monitise.com>
2018-07-08 13:51:37 +0300

    Include cron definition in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@
 sudo apt install python3-pip
 
 pip3 install -r requirement.txt
+
+cmd="/home/terryadmin/bamboo-timeoff-reminder/remind_timeoffs.sh"
+job="0 6 * * MON $cmd"
+
+( crontab -l | grep -v -F "$cmd" ; echo "$job" ) | crontab -
