4854a67a97501ea8cc56f0863e1a609daca5c3df
Can Elmas <Can.Elmas@monitise.com>
2019-06-11 17:36:18 +0300

    Add filename and lineno to log format Set logger encoding to utf-8

--- logger.py
+++ logger.py
@@ -2,12 +2,12 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 LOG_FILE = "logs/bamboo-timeoffs.log"
-FORMAT = "%(asctime)s %(levelname)s %(name)s: %(message)s"
+FORMAT = "%(asctime)s %(levelname)s %(name)s [%(filename)s:%(lineno)d]: %(message)s"
 
 logging.basicConfig(level=logging.INFO,
                     format=FORMAT,
                     datefmt='%m-%d-%Y %H:%M',
-                    handlers=[RotatingFileHandler(LOG_FILE, maxBytes=10 * 1024 * 1024, backupCount=3)])
+                    handlers=[RotatingFileHandler(LOG_FILE, maxBytes=10 * 1024 * 1024, backupCount=3, encoding="utf-8")])
 
 
 def get_logger():
